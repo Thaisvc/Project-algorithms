b0f5502f362d7984aca7abb8a626999bdfb28743
thais viana <thaisviana081@outlook.com>
2023-03-23 10:26:23 -0300

    1 - Número de estudantes estudando no mesmo horário (Algoritmo de busca)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-   
+    try:
+        period_count = 0
+        for start, end in permanence_period:
+            if target_time >= start and target_time <= end:
+                period_count += 1
+    except TypeError:
+        return None
+    return period_count
