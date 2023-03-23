2ab50520099151e64bb8fbb3285984f9306c3922
thais viana <thaisviana081@outlook.com>
2023-03-23 10:37:30 -0300

    Criptografia de inversões (Testes)

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    #  retorna a mensagem corretamente com chaves pares
+    assert encrypt_message('thais', 2) == 'sia_ht'
+    # retorna a mensagem corretamente com chaves ímpares
+    assert encrypt_message('thais', 3) == 'aht_si'
+    assert encrypt_message('thais', 5) == 'siaht'
+    with pytest.raises(TypeError):
+        encrypt_message('thais', '1')
+    with pytest.raises(TypeError):
+        encrypt_message(2, 4)
